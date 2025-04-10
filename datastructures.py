5f4e57bdaca7ba9fdb42709b83f0a8be349a0466
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-10 16:48:22 +0300

    added new content

--- datastructures.py
+++ datastructures.py
@@ -26,3 +26,43 @@
         We can do this:
             students = {"Tilak" : 18,"Ansh" : 18,"Esha" : 20}
 """
+
+"""
+= python data structures come with powerfull builts-in methods
+=advance data structures like queues, stacks, graphs and trees can be built
+ using lists, dictonaries or classes. Understanding basic data structure 
+ prepare you for more complex ones
+"""
+
+
+#LISTS
+"""
+    lists are ordered collections that contain eletments of different data types
+    Properties:
+        * Ordered
+        * Mutable
+        * They allow duplicates
+"""
+
+name = ["Esha", "Tilak", "Saleh", "Vansh", "Ansh", "Natasha", "Ansh" ]
+#INDEXING
+print(name[0]) #Esha
+#SLICING
+print(name[1:2]) #'Tilak'
+#ADDING
+name.append("Janet") #["Esha", "Tilak", "Saleh", "Vansh", "Ansh", "Natasha", "Ansh", "Janet" ]
+#INSERTING
+name.insert(1, "Joseph") #["Esha", "Joseph", "Tilak", "Saleh", "Vansh", "Ansh", "Natasha", "Ansh", "Janet" ]
+#REMOVING
+name.remove(0) #["Joseph", "Tilak", "Saleh", "Vansh", "Ansh", "Natasha", "Ansh", "Janet" ]
+#POP
+last_item = name.pop() #Janet
+#LOOPING
+for name in name:
+    print(name) #Joseph, Tilak, Saleh, Vansh, Ansh,
+#LENGTH
+print(len(name)) #7
+#LIST METHODS
+name.sort() #sorts inn ascending order
+name.reverse() #reverses the list
+
