c6f4c48809a211fc45564a46cf051a85d7f8b1fc
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-10 16:52:37 +0300

    added a new list

---
+++ cars.py
@@ -0,0 +1,6 @@
+#create a list of 5 car models
+car_models = ["Toyota", "Honda", "Ford", "Nissan", "Chevrolet"]
+# Remove the last item/car model
+car_models.pop()
+#print the car models lists
+print(car_models)
