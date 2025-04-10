23d7318eb1c3512fe775a5f21167c6098b9baaa0
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-10 17:04:46 +0300

    created a tuple movie exercise

---
+++ movies.py
@@ -0,0 +1,7 @@
+#create a tuople with 5 movies
+movies = ("dora","jadu","PK","PK2","merry jaan")
+#Access the second item and update the movie name 
+print(movies[1])
+#explain what happens and why?
+#The tuple is immutable, so we can't update the movie name directly. However, we can
+
