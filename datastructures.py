a05ddd3d0c8ca5baee5be38483cbc2fb807fa6e8
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-10 16:29:49 +0300

    added a new py file

---
+++ datastructures.py
@@ -0,0 +1,28 @@
+# WHY DATA STRUCTURES MATTER
+
+"""
+= They organize, store and manage data in ways that make your program more efficient, readable, and powerful.
+= Diffrent problems need diffrent way to store and retrieve data.
+= Lists are a great way for ordered collection you change .
+= Tuples are like lists but immutable, perfect for mixed data.
+= Dictionaries gives you fast access to data using keys.
+= Using the right data structures makes your code faster.
+= Looking up at an item in a dictionary is much faster thab a searching list
+"""
+"""
+= They organize, store and manage data in ways that make your program more efficient, readable, and powerful.
+= Diffrent problems need diffrent way to store and retrieve data.
+= Lists are a great way for ordered collection you change .
+= Tuples are like lists but immutable, perfect for mixed data.
+= Dictionaries gives you fast access to data using keys.
+= Using the right data structures makes your code faster.
+= Looking up at an item in a dictionary is much faster thab a searching list.
+= A set is great when you want to remove duplicates.
+= Well chosen data structures make code easier to understand
+    e.g 
+    instead of:-
+            names = ["Tilak", "Ansh", "Esha"]
+            ages = ["18", "18", "20"]
+        We can do this:
+            students = {"Tilak" : 18,"Ansh" : 18,"Esha" : 20}
+"""
