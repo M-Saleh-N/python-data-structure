a48aca446555de0a45290a8c1ac919b28440b5f1
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-10 16:59:15 +0300

    added notes and example

---
+++ tuple.py
@@ -0,0 +1,21 @@
+# TUPLE
+"""
+= Tuples are similar to list but immutable (cannot change) once created 
+Properties:-
+    * Ordered
+    * Immutable
+    * Faster than list
+    * Good for fixed data
+
+"""
+names = ("Saleh", "Vansh", "Tilak", "Ansh", "Esha")
+# INDEXING
+print(names[0])  # Saleh
+# Tuple unpacking
+a,b,c,d,e = names
+print(a)  # Saleh 
+"""
+        Where to use:
+            * Coordinate
+            * Function return value
+"""
